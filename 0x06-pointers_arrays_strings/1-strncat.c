9ecb9b4d7fe49ed71f1b81e420ea3a9348368302
Tromba22 <alitrabelsikaroui2293@gmail.com>
2023-03-02 16:55:52 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,24 +1,29 @@
 #include "main.h"
+
 /**
- * _strncpy - Write a function that copies a string, with number of bytes
- * @dest: This is the output dest
- * @src: This is the input source
- * @n: This is the number of bytes to copy
+ * _strncat - Write a function that concatenates two strings. it will use at
+ *            most n bytes from src
  *
- *Return: Copied string with the number of bytes
+ * @dest: This is the output  dest
+ * @src: This is the input src
+ * @n: This is the number bytes fro src
+ *
+ * Return: This is my return
  */
-
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	int index;
+	int index, count;
 
-	for (index = 0; index < n && src[index] != '\0' ; index++)
+	for (index = 0; dest[index] != '\0'; index++)
+		;
+	for (count = 0; count < n && src[count] != '\0'; count++)
 	{
-		dest[index] = src[index];
+		dest[index] = src[count];
+		index++;
 	}
-	for (; index  < n; index++)
+	if (count < n)
 	{
-		dest[index] = '\0';
+	dest[index] = '\0';
 	}
 	return (dest);
 }
